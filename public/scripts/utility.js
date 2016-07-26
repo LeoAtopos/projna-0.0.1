895b23494106dc223ea2ab929ba70413b8f926eb
LeoAtopos <275142760@qq.com>
2016-07-27 00:31:40 +0800

    get checkSession down

--- public/scripts/utility.js
+++ public/scripts/utility.js
@@ -1,1 +1,25 @@
-<
+
+$(document).ready(checkLogIn);
+function checkLogIn(){
+	if(isLogged()){
+		$("#login").hide();
+		$("#beProjna").hide();
+		$("#projnaName").show();
+	}
+	else{
+		$("#login").show();
+		$("#beProjna").show();
+		$("#projnaName").hide();
+	}
+}
+function isLogged(){
+	var userInfo;
+	$.ajax({
+		type:"GET",url:"http://127.0.0.1:8081/user/checkSession",data:{},
+		success: function(msg){
+			userInfo = msg;
+		}
+	});
+	if (userInfo) return true;
+	else return false;
+}
