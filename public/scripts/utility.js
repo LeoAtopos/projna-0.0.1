48f4a3c96cd9f4bbae413e5c95af81f34d6722ab
LeoAtopos <275142760@qq.com>
2016-07-28 10:29:33 +0800

    delete those stupid ips

--- public/scripts/utility.js
+++ public/scripts/utility.js
@@ -4,7 +4,7 @@ function checkLogIn(){
 	var userInfo;
 	$.ajax({
 		// type:"GET",url:"http://172.16.40.36:8081/user/checkSession",data:{},
-		type:"GET",url:"http://127.0.0.1:8081/user/checkSession",data:{},
+		type:"GET",url:"/user/checkSession",data:{},
 		success: function(msg){
 			userInfo = msg;
 			if(userInfo.status === "user"){
