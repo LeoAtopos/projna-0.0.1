9612a1c6682ba1e04b9a3cb61da7c0736ea23f96
LeoAtopos <275142760@qq.com>
2016-08-26 13:01:24 +0800

    test a little

--- public/scripts/utility.js
+++ public/scripts/utility.js
@@ -1,3 +1,20 @@
+pageData = {
+	login : "visitor",//"projna"
+	location : "projna",//"projna","project","featrue","build","building"
+	person : "visitor",//"person"
+	projna : [
+		{id : "bookseeds", name : "Book Seeds", pic : "Pro1-pic", state : "intro"},
+		{id : "graveyard", name : "Grave Yard", pic : "Pro2-pic", state : "intro"}
+	],
+	project : {
+		id : "bookseeds",//"project"
+		data : {}
+	},
+	build : {
+		id : "sample",
+		data : {}
+	}
+}
 
 $(document).ready(checkLogIn);
 function checkLogIn(){
@@ -11,13 +28,15 @@ function checkLogIn(){
 				$("#login").hide();
 				$("#beProjna").hide();
 				$("#projnaName").show();
-				alert(JSON.stringify(userInfo));
+				// alert(JSON.stringify(userInfo));
 				$("#mplink").attr("href","/myprojects/" + userInfo.session.email);
+				pageData.login = userInfo.session.email;
 			}
 			else{
 				$("#login").show();
 				$("#beProjna").show();
 				$("#projnaName").hide();
+				pageData.login = "visitor";
 			}
 		}
 	});
